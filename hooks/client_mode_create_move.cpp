61839ef1bcc2df985b59b3a0a32f1d1b8540a523
mlemlody <garciaisheree@gmail.com>
2025-08-17 05:54:33 +0200

    now it works fine

--- hooks/client_mode_create_move.cpp
+++ hooks/client_mode_create_move.cpp
@@ -225,13 +225,12 @@ bool client_mode_create_move_hook(void* me, float sample_time, user_cmd* user_cm
                 float next_yaw = current_yaw + (delta_yaw / aim_speed);
                 next_yaw = ang_norm(next_yaw);
                 user_cmd->view_angles.y = next_yaw;
-                s_nav_last_look_angles = user_cmd->view_angles;
-                s_nav_look_applied = true;
               } else {
                 user_cmd->view_angles.y = ang_norm(desired_yaw);
-                s_nav_last_look_angles = user_cmd->view_angles;
-                s_nav_look_applied = true;
               }
+              user_cmd->view_angles.x = 0.0f;
+              s_nav_last_look_angles = user_cmd->view_angles;
+              s_nav_look_applied = true;
             }
           }
         }
