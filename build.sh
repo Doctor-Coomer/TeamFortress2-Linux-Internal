649577af8c92c736a6527dc01169700464473ca6
Dr_Coomer <isfortner@gmail.com>
2025-08-27 18:54:04 -0400

    Make package script less unified

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#replace Arch with distro name
-distroname=$(lsb_release -is)
-sed -i 's/Arch/'${distroname}'/g' hooks/sdl.cpp
-
 # Clone and build funchook
 if [ -d "libs/funchook" ]; then
     rm -rf libs
