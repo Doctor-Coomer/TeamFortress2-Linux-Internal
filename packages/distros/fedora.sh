10a71d535c5f9d5ba1fac3a12c902367c83990a3
linuxgamer <stasterra2@gmail.com>
2025-08-31 15:06:09 +0400

    fix libGLEW 2.1
    fedora doesnt have libGLEW 2.1 so i used .rpm package

--- packages/distros/fedora.sh
+++ packages/distros/fedora.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#bug: installs libGLEW 2.2 while building needs libGLEW 2.1
-dnf -y install git cmake gdb gcc g++ make SDL2-devel glew-devel libGLEW
+dnf -y install git cmake gdb gcc g++ make SDL2-devel glew-devel
+wget https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.6/repo/oss/x86_64/libGLEW2_1-2.1.0-1.28.x86_64.rpm #fedora doesnt have libglew 2.1
+rpm -ivh libGLEW2_1-2.1.0-1.28.x86_64.rpm
 dnf -y group install "c-development"
 dnf -y group install "development-tools"
