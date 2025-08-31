0596da81688f54479f39e072d6e35c0d570b7fdf
linuxgamer <stasterra2@gmail.com>
2025-08-31 15:25:02 +0400

    Revert packages.sh
    oops

--- packages/packages.sh
+++ packages/packages.sh
@@ -3,13 +3,13 @@
 {
   if [ -f /etc/arch-release ]; then
     echo "Detected Arch Linux or derivative"
-    ./distros/arch.sh
+    ./packages/distros/arch.sh
   elif [ -f /etc/debian_version ]; then
     echo "Detected Debian/Ubuntu or derivative"
-    ./distros/debian.sh
+    ./packages/distros/debian.sh
   elif [ -f /etc/redhat-release ]; then
     echo "Detected Fedora/CentOS"
-    ./distros/fedora.sh
+    ./packages/distros/fedora.sh
   else
     echo "Unsupported Linux distribution."
     exit 1
