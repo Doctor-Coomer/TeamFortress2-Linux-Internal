413baf527d9d316baa79556dec627ef373984224
linuxgamer <stasterra2@gmail.com>
2025-08-31 15:14:40 +0400

    Fix package installation (again)
    added sudo to all dnf commands

--- packages/distros/fedora.sh
+++ packages/distros/fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dnf -y install git cmake gdb gcc g++ make SDL2-devel glew-devel
+sudo dnf -y install git cmake gdb gcc g++ make SDL2-devel glew-devel
 wget https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.6/repo/oss/x86_64/libGLEW2_1-2.1.0-1.28.x86_64.rpm #fedora doesnt have libglew 2.1
-rpm -ivh libGLEW2_1-2.1.0-1.28.x86_64.rpm
-dnf -y group install "c-development"
-dnf -y group install "development-tools"
+sudo rpm -ivh libGLEW2_1-2.1.0-1.28.x86_64.rpm
+sudo dnf -y group install "c-development"
+sudo dnf -y group install "development-tools"
