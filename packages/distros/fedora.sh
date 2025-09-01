1183bd5923e7bad1c379976fe1d9fb7cbc6e6473
linuxgamer <stasterra2@gmail.com>
2025-09-01 09:09:34 +0000

    Update fedora.sh
    remove rpm after installaton

--- packages/distros/fedora.sh
+++ packages/distros/fedora.sh
@@ -2,5 +2,6 @@
 sudo dnf -y install git cmake gdb gcc g++ make SDL2-devel glew-devel
 wget https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.6/repo/oss/x86_64/libGLEW2_1-2.1.0-1.28.x86_64.rpm #fedora doesnt have libglew 2.1
 sudo rpm -ivh libGLEW2_1-2.1.0-1.28.x86_64.rpm
+rm libGLEW2_1-2.1.0-1.28.x86_64.rpm
 sudo dnf -y group install "c-development"
 sudo dnf -y group install "development-tools"
